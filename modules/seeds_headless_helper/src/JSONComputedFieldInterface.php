990d8ad0bbf77de9e4135b65ac28a97315b3c141
ahmad-alyasaki <a.alyasaki@sprintive.com>
2023-05-23 16:52:52 +0300

    Issue #3362144 by Ahmad Alyasaki: Computed Languages Normalizer

---
+++ modules/seeds_headless_helper/src/JSONComputedFieldInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\seeds_headless_helper;
+
+/**
+ * This interface is only for JSON API normalization.
+ */
+interface JSONComputedFieldInterface {
+}
---
+++ modules/seeds_headless_helper/src/Normalizer/ComputedLanguagesNormalizer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\seeds_headless_helper\Normalizer;
+
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\TypedData\Plugin\DataType\StringData;
+use Drupal\seeds_nextjs\JSONComputedFieldInterface;
+use Drupal\serialization\Normalizer\NormalizerBase;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * The computed languages normalizer.
+ */
+class ComputedLanguagesNormalizer extends NormalizerBase implements NormalizerInterface {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected $supportedInterfaceOrClass = FieldItemListInterface::class;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function supportsNormalization($data, $format = NULL) {
+    if ($data instanceof StringData) {
+      return $data->getParent()->getParent() instanceof JSONComputedFieldInterface;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function normalize($object, $format = NULL, array $context = []) {
+    if ($object instanceof CacheableDependencyInterface) {
+      $cacheability = $context["cacheability"] ?? NULL;
+      if ($cacheability instanceof CacheableMetadata) {
+        $cacheability->addCacheableDependency($object);
+      }
+    }
+    /** @var \Drupal\Core\TypedData\Plugin\DataType\StringData $object */
+    return json_decode($object->getValue(), TRUE);
+  }
+
+}
